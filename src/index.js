f427c54ef09a6769f90a06bf05d607a32aea5378
Nelson Gonzalez G <nelson.gonzalez.g@hotmail.com>
2022-05-08 16:13:48 -0500

    Primer Commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import _ from 'lodash';
+import "@css/normalize.css";
+import "@css/style.css";
+import "@js/imagenes.js";
+import "@img/bg-precio.avif";
+import "@img/bg-precio.jpg";
+import "@img/bg-precio.webp";
+import "@img/grafico-audifonos.png";
+import "@img/header.avif";
+import "@img/header.png";
+import "@img/header.webp";
+import "@img/icono-bateria.svg";
+import "@img/icono-garantia.svg";
+import "@img/icono-sonido.svg";
+import "@img/imagen-mujer.avif";
+import "@img/imagen-mujer.jpg";
+import "@img/imagen-mujer.webp";
+import "@img/modelo-x.svg";
+import "@img/modelo-y.svg";
+import "@img/modelo-z.svg";
+import "@img/newsletter.avif";
+import "@img/newsletter.jpg";
+import "@img/newsletter.webp";
+
+
